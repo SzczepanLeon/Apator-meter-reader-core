7846ce1eaf477193517016d15b1f2302ac21f3a3
SzczepanLeon <75840303+SzczepanLeon@users.noreply.github.com>
2022-12-25 22:58:58 +0100

    Update wmbus_utils.cc

--- wmbus_utils.cc
+++ wmbus_utils.cc
@@ -33,49 +33,21 @@ bool decrypt_TPL_AES_CBC_IV(vector<uchar> &frame,
     *num_encrypted_bytes = num_bytes_to_decrypt;
     *num_not_encrypted_at_end = buffer.size()-num_bytes_to_decrypt;
 
-    char cs[100];
-    sprintf(cs, "(TPL) num encrypted blocks %zu (%d bytes and remaining unencrypted %zu bytes)\n",
-             tpl_num_encr_blocks, num_bytes_to_decrypt, buffer.size()-num_bytes_to_decrypt);
-    Serial.print(cs);
 
     if (key.size() == 0) return false;
-
-//    debugPayload("(TPL) AES CBC IV decrypting", buffer);
-//    s = bin2hex(buffer);
-//    Serial.print("(TPL) AES CBC IV decrypting "); Serial.println(s.c_str());
 
 
     if (buffer.size() < num_bytes_to_decrypt)
     {
-//        warning("(TPL) warning: decryption received less bytes than expected for decryption! "
-//                "Got %zu bytes but expected at least %zu bytes since num encr blocks was %d.\n",
-//                buffer.size(), num_bytes_to_decrypt,
-//                tpl_num_encr_blocks);
-        sprintf(cs, "(TPL) warning I ... %zu %zu %d.\n",
-                    buffer.size(), num_bytes_to_decrypt,  tpl_num_encr_blocks);
-        Serial.print(cs);
+
         num_bytes_to_decrypt = buffer.size();
     }
 
     // The content should be a multiple of 16 since we are using AES CBC mode.
     if (num_bytes_to_decrypt % 16 != 0)
     {
-//        warning("(TPL) warning: decryption received non-multiple of 16 bytes! "
-//                "Got %zu bytes shrinking message to %zu bytes.\n",
-//                num_bytes_to_decrypt, num_bytes_to_decrypt - num_bytes_to_decrypt % 16);
-//        num_bytes_to_decrypt -= num_bytes_to_decrypt % 16;
-          sprintf(cs, "(TPL) warning II ... %zu %zu %d.\n",
-                  num_bytes_to_decrypt, num_bytes_to_decrypt - num_bytes_to_decrypt % 16);
-        Serial.print(cs);
         assert (num_bytes_to_decrypt % 16 == 0);
     }
-
-    std::vector<uchar> ivv(iv, iv+16);
-    s = bin2hex(ivv);
-    Serial.printf("(TPL) ivv %s\n",s.c_str());
-    s = bin2hex(key);
-    Serial.printf("(TPL) key %s\n",s.c_str());
-//    debug("(TPL) IV %s\n", s.c_str());
 
     uchar buffer_data[num_bytes_to_decrypt];
     memcpy(buffer_data, safeButUnsafeVectorPtr(buffer), num_bytes_to_decrypt);
@@ -99,17 +71,9 @@ bool decrypt_TPL_AES_CBC_IV(vector<uchar> &frame,
     //frame.insert(frame.end(), decrypted_data, decrypted_data+num_bytes_to_decrypt);
     frame.insert(frame.end(), decrypted_data, decrypted_data+num_bytes_to_decrypt);
 
-
-    s = bin2hex(buffer_data, num_bytes_to_decrypt);
-    Serial.printf("(TPL) data %s\n", s.c_str());
-
-    s = bin2hex(decrypted_data, num_bytes_to_decrypt);
-    Serial.printf("(TPL) decr %s\n", s.c_str());
-
     if (num_bytes_to_decrypt < buffer.size())
     {
         frame.insert(frame.end(), buffer.begin()+num_bytes_to_decrypt, buffer.end());
-        Serial.printf("(TPL) appended ... \n");
     }
     return true;
 }
